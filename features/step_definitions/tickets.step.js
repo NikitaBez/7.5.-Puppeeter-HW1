8443e7a0f38280af119b2d53fdab53fdd8de82f9
NikitaBez <bezmenov.nikita@gmail.com>
2023-05-27 22:05:10 +0900

    modified cucumber tests

--- features/step_definitions/tickets.step.js
+++ features/step_definitions/tickets.step.js
@@ -1,19 +1,29 @@
+const {
+  Given,
+  When,
+  Then,
+  Before,
+  After,
+  setDefaultTimeout,
+} = require("cucumber");
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { When, Then, Before, After, setDefaultTimeout } = require("cucumber");
 const { clickElement, getText } = require("../../lib/commands.js");
 
 setDefaultTimeout(60000);
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
-  await page.goto("https://qamid.tmweb.ru");
-  await clickElement(page, "a:nth-child(3) > span.page-nav__day-week");
-  await clickElement(page, "a.movie-seances__time");
+  await this.page.goto("https://qamid.tmweb.ru");
+  await clickElement(this.page, "a:nth-child(3) > span.page-nav__day-week");
+  await clickElement(
+    this.page,
+    ".movie .movie-seances__hall .movie-seances__time"
+  );
 });
 
 After(async function () {
@@ -23,21 +33,21 @@ After(async function () {
 });
 
 When(
-  "user clicks on the next date, and the first available time, selects the first available place, click on Забронировать button",
-  async function (string) {
+  "user clicks on the next date, and the first available time, on {int} row and {int} chair and click on Забронировать button",
+  async function (row, chair) {
     await clickElement(
       this.page,
-      ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken"
+      `div:nth-child(${row}) > span:nth-child(${chair})`
     );
     return await clickElement(this.page, "button.acceptin-button");
   }
 );
 
 Then(
-  "the user sees a page with the results of booking tickets {string}",
+  "the user sees a page with the results of booking tickets with Row / Chair {string}",
   async function (string) {
     const actual = await getText(this.page, "h2.ticket__check-title");
-    const expected = await string;
+    const expected = string;
     expect(actual).contains(expected);
   }
 );
