b31e5b9a0b5ac471dd55180c553302f1d02cbd3a
NikitaBez <bezmenov.nikita@gmail.com>
2023-06-06 21:10:53 +0900

    tickest.step.js is modefied

--- features/step_definitions/tickets.step.js
+++ features/step_definitions/tickets.step.js
@@ -32,7 +32,7 @@ When(
       this.page,
       `div:nth-child(${row}) > span:nth-child(${chair})`
     );
-    return await clickElement(this.page, "button.acceptin-button");
+    return await clickElement(this.page, ".acceptin-button");
   }
 );
 
@@ -54,15 +54,33 @@ When(
       this.page,
       `div:nth-child(${row2}) > span:nth-child(${chair2})`
     );
-    return await clickElement(this.page, "button.acceptin-button");
+    await clickElement(
+      this.page,
+      `div:nth-child(${row2}) > span:nth-child(${chair2})`
+    );
+    return await clickElement(this.page, ".acceptin-button");
   }
 );
 
 Then(
-  "the user sees a page with the results of booking tickets with Row / Chair: {string}', function (string)",
+  "the user sees a page with the results of booking tickets with Row / Chair: {string}",
   async function (string) {
     const actual = await getText(this.page, ".ticket__chairs");
     const expected = string;
     expect(actual).contains(expected);
   }
 );
+
+When(
+  "user clicks on the next date, and the first available time, on {string} chair and click on Забронировать button",
+  async function (chair) {
+    await clickElement(this.page, `div:buying-scheme__chair(${chair})`);
+    return await clickElement(this.page, ".acceptin-button");
+  }
+);
+Then("the button Забронировать is desabled", async function (string) {
+  const actual = await this.page.$eval(".acceptin-button", (link) =>
+    link.getAttribute("disabled")
+  );
+  expect(actual).equal("true");
+});
